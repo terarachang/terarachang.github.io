891379fd4ffbfadcae16770559c01469bbbf4dfb
terarachang <qqq09289@gmail.com>
2023-11-28 17:32:16 -0800

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Personal Website
+- Templates from https://github.com/jonbarron/website and https://github.com/tsujuifu/tsujuifu.github.io
---
+++ imgs/fill.py
@@ -0,0 +1,14 @@
+import os, sys
+import numpy as np
+from PIL import Image
+
+if __name__=='__main__':
+    file = sys.argv[1]
+    img = np.array(Image.open(file))
+    H, W = img.shape[:2]
+    nH, nW = int(W*150/250), W
+    print(H, W, nH, nW, img.shape)
+
+    out = np.ones((nH, nW, 3), dtype='uint8')*255
+    out[(nH-H)//2:(nH-H)//2+H, :W, :] = img[:, :, :3]
+    Image.fromarray(out).save(file)
